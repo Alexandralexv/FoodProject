5448fd124be9ed32bf6fd16c2d9c0ae527f9a3dc
Alex Semionov <alexandralexv@gmail.com>
2024-10-06 19:26:08 +0300

    RibbonMenu module

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,15 +1,25 @@
 import Carousel from "../blocks/Carousel.js";
 import slides from "../storages/slides.js";
 
+import RibbonMenu from "../blocks/RibbonMenu.js";
+import categories from "../storages/categories.js";
+
 export default class Main {
   constructor() {}
 
   async render() {
     this.renderCarousel();
+    this.renderRibbon();
   }
 
   renderCarousel() {
     this.carousel = new Carousel(slides);
     document.querySelector("[data-carousel-holder]").append(this.carousel.elem);
+  }
+
+  renderRibbon() {
+    this.ribbonMenu = new RibbonMenu(categories);
+
+    document.querySelector("[data-ribbon-holder]").append(this.ribbonMenu.elem);
   }
 }
